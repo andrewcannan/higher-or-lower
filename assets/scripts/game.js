bcefd408fcdd335be40e8626f254514c2ca1f8e6
andrewcannan <andrewmcannan@gmail.com>
2023-04-23 11:48:27 +0000

    added functionality to hide first two divs

---
+++ assets/scripts/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#rules-button').click(function() {
+        $('#first-paint').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('#start-button').click(function() {
+        $('#rules').hide();
+    });
+});
