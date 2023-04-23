426d2b999f409acae6da3f680cf2828fe319f246
andrewcannan <andrewmcannan@gmail.com>
2023-04-23 13:47:14 +0000

    styled first div/page and improved hide/show functionality

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
+    $('#rules').hide();
+    $('#game').hide();
     $('#rules-button').click(function() {
         $('#first-paint').hide();
+        $('#rules').show();
+    });
+    $('#start-button').click(function() {
+        $('#rules').hide();
+        $('#game').show();
     });
 });
 
